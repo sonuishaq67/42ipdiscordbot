0eeab6589d906ee80f1be96fd1217e9604ea091a
si67 <sonuishaq67@gmail.com>
2020-09-16 15:11:30 +0530

    more

--- bot.py
+++ bot.py
@@ -17,15 +17,15 @@ async def on_message(message):
         return
 
     if "what can you do" in message.content:
-        msg = 'Hello {0.author.mention}'.format(message)
+        msg = 'For now only 4-5 tasks {0.author.mention}'.format(message)
         await channel.send(f'{msg}')
-    elif "bot" in message.content:
-        await channel.send('Im not a fricking bot :kawai:')
+    elif "hi bot" in message.content:
+        await channel.send('Hey man {0.author.mention}'.format(message))
     elif "introduce yourself" in message.content:
-        msg = 'Hello @everyone I am java.awt'
+        msg = "Hello @everyone I am 42ip's bot"
         await channel.send(f'{msg}')
     elif "noice" in message.content:
-        await channel.send('Hello', file=discord.File('tenor.gif'))
+        await channel.send(file=discord.File('tenor.gif'))
     elif "i love democracy" in message.content:
         await channel.send(file=discord.File('democracy.gif'))
     elif "can you reply to yourself" in message.content:
@@ -35,9 +35,11 @@ async def on_message(message):
         msg = "oh can you reply to yourself"
         await channel.send(f'{msg}')
     elif "am i ugly" in message.content.lower():
-        replies = ["It runs in your fam bitch", "Nah man you beautiful", "Yes.","What if I told no"]
-        n = random.randint(0,len(replies)-1)
+        replies = ["It runs in your fam bitch",
+                   "Nah man you beautiful", "Yes.", "What if I told no"]
+        n = random.randint(0, len(replies)-1)
         await channel.send(f'{replies[n]}')
+
 
 @client.event
 async def on_ready():
