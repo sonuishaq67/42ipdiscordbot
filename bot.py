7d5565534a3090dc8d061cfa2a9d71506bf085fd
si67 <sonuishaq67@gmail.com>
2020-09-16 10:14:49 +0530

    ugly

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 import discord
 from dotenv import load_dotenv
 
@@ -8,20 +8,37 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
 @client.event
 async def on_message(message):
-    channel = client.get_channel(714036401334911036)
+    print(message)
+    channel = client.get_channel(message.channel.id)
     if message.author == client.user:
         return
 
     if "what can you do" in message.content:
         msg = 'Hello {0.author.mention}'.format(message)
         await channel.send(f'{msg}')
-    if "introduce yourself" in message.content:
+    elif "bot" in message.content:
+        await channel.send('Im not a fricking bot :kawai:')
+    elif "introduce yourself" in message.content:
         msg = 'Hello @everyone I am java.awt'
         await channel.send(f'{msg}')
-    if "noice" in message.content:
+    elif "noice" in message.content:
         await channel.send('Hello', file=discord.File('tenor.gif'))
+    elif "i love democracy" in message.content:
+        await channel.send(file=discord.File('democracy.gif'))
+    elif "can you reply to yourself" in message.content:
+        msg = 'yes i can'
+        await channel.send(f'{msg}')
+    elif "yes i can" in message.content:
+        msg = "oh can you reply to yourself"
+        await channel.send(f'{msg}')
+    elif "am i ugly" in message.content.lower():
+        replies = ["It runs in your fam bitch", "Nah man you beautiful", "Yes.","What if I told no"]
+        n = random.randint(0,len(replies)-1)
+        await channel.send(f'{replies[n]}')
+
 @client.event
 async def on_ready():
     print('Logged in as')
