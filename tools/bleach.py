35cb590837f54904dd50f74ddbb7f1e78cbd86b5
si67 <sonuishaq67@gmail.com>
2020-10-01 15:55:43 +0530

    import random and remove regex

--- tools/bleach.py
+++ tools/bleach.py
@@ -1,15 +1,17 @@
 import re
+import random
+
 
 def get_bleach(r: str):
-	"""
-	The get bleach function revibes a route r
-	and returns one of their lines, this function has been created to 
-	mainly work with the bleach.txt file.
-	"""
-	with open(r) as file:
-		try:
-			msg = random.choice(file.readlines())
-			return re.search(r"\[([A-Za-z0-9_]+)\]", '{}'.format(msg))
-		
-		except:
-			return 'Error in the get_bleach() function'
+    """
+    The get bleach function revibes a route r
+    and returns one of their lines, this function has been created to 
+    mainly work with the bleach.txt file.
+    """
+    with open(r) as file:
+        try:
+            msg = random.choice(file.readlines())
+            return format(msg)
+
+        except:
+            return 'Error in the get_bleach() function'
