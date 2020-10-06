47581825a1a2a87bee40e29bd6cdd52a0cbb43bb
si67 <sonuishaq67@gmail.com>
2020-10-06 11:25:23 +0530

    its like c

--- main.sh
+++ main.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+stop_process() {
+    process_id=$(ps -ef | grep "bot.py" | cut -b 12-16 | head -n 1)
+    kill -9 $process_id
+}
+
+run_again() {
+    cd ~/TARS
+    rm nohup.out
+    nohup ./bot.py &
+}
+
+send_mail() {
+    cat ~/TARS/.pull >>$MAILFILE
+    cat $MAILFILE | /usr/sbin/ssmtp ishaqshaik084@gmail.com
+}
 
 # to check every 15 minutes for a new version in git
 
@@ -21,19 +36,3 @@ elif [ "$c3" -gt 0 ]; then
     echo -e "Subject: Theres a pulling error for TARS @ $(date)\nTo: ishaqshaik084@gmail.com" >$MAILFILE
     send_mail
 fi
-
-stop_process() {
-    process_id=$(ps -ef | grep "bot.py" | cut -b 12-16 | head -n 1)
-    kill -9 $process_id
-}
-
-run_again() {
-    cd ~/TARS
-    rm nohup.out
-    nohup ./bot.py &
-}
-
-send_mail() {
-    cat ~/TARS/.pull >>$MAILFILE
-    cat $MAILFILE | /usr/sbin/ssmtp ishaqshaik084@gmail.com
-}
