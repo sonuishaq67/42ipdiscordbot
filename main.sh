825f227996da40652ad10b33f6a6fd16b2ed5799
si67 <sonuishaq67@gmail.com>
2020-10-06 11:21:47 +0530

    man

--- main.sh
+++ main.sh
@@ -11,11 +11,13 @@ if [ "$c1" -gt 0 ]; then
     echo "already upto date"
     exit
 elif [ "$c2" -gt 0 ]; then
+    echo "updating"
     stop_process
     run_again
     echo -e "Subject: Rerun TARS @ $(date)\nTo: ishaqshaik084@gmail.com" >$MAILFILE
     send_mail
 elif [ "$c3" -gt 0 ]; then
+    echo "error"
     echo -e "Subject: Theres a pulling error for TARS @ $(date)\nTo: ishaqshaik084@gmail.com" >$MAILFILE
     send_mail
 fi
