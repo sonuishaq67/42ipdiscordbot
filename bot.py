de40a9003d1f211a3b7ab499da8ace7ea7534a4d
si67 <sonuishaq67@gmail.com>
2020-10-22 11:25:52 +0530

    maybe

--- bot.py
+++ bot.py
@@ -91,9 +91,8 @@ async def on_message(message):
     elif message.content.startswith("/doge"):
         api_url="http://shibe.online/api/shibes?count=1"
         response=await fetch_data(api_url)
-        print(message.channel.recipient.name)
         embed=discord.Embed(
-            title=f"doge for {message.channel.recipient.name}",
+            title=f"doge for {message.author}",
         )
         embed.set_image(url=f"{response[0]}")
         await channel.send(embed=embed)
